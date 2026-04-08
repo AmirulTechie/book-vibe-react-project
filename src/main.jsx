158a1d35337b90a5319d875c6f0481c002d7d4c9
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-04-08 11:48:01 +0600

    chore: structure the folder formation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import MainLayout from './layout/MainLayout'
+import Homepage from './pages/homepage/Homepage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        index: true,
+        element: <Homepage></Homepage>
+      },
+      // {
+      //   path: '/books',
+      //   element: 
+      // }
+    ]
+  },  {
+    path: '/books',
+    element: 'Books'
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h2>Hello</h2>
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
